50add84501476d6fdf606d33b87c9c9f4c2d1e3a
wilglison <wilgilson.niky@gmail.com>
2024-02-28 19:23:31 -0300

    feature: inclui a data do ultimo boot

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ PASSWORD = os.environ.get('PASSWORD')
 IP_PRINTS = os.environ.get('IP_PRINTS').split(",")
 TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
+niveis_tooner=[]
 saida="Nível de tooner das impressoras:\n"
 
 chrome_options = webdriver.ChromeOptions()
@@ -23,7 +24,7 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 
 def reinicia(ip_print):
     driver.get(f"https://{LOGIN}:{PASSWORD}@{ip_print}/#hId-pgPowerCycle")
-    WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, '//input[@type="button"]'))).click()
+    WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '//input[@type="button"]'))).click()
     driver.find_element(By.CLASS_NAME, "gui-cmd-bar-btn-power_cycle").click()
     driver.implicitly_wait(3)
     driver.find_element(By.XPATH, "//button[@class='gui-action-btn' and text()='Sim']").click()
@@ -31,9 +32,11 @@ def reinicia(ip_print):
 
 def nivel_tooner(ip_print):
     driver.get(f"https://{ip_print}/#hId-pgConsumables")
-    xpath="/html/body/div[1]/div[5]/div[1]/div[2]/form/div[1]/div[2]/div[1]/div/table/tbody/tr[8]/td[2]"
-    pg_restantes=WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, xpath)))
-    return ip_print+" "+pg_restantes.text
+    xpath_nivel="/html/body/div[1]/div[5]/div[1]/div[2]/form/div[1]/div[2]/div[1]/div/table/tbody/tr[8]/td[2]"
+    xpath_boot="/html/body/div[1]/div[5]/div[1]/div[2]/form/div[1]/div[2]/div[1]/div/table/tbody/tr[4]/td[2]"
+    pg_restantes=WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, xpath_nivel)))
+    ultima_boot=WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, xpath_boot)))
+    return [ip_print,pg_restantes.text,ultima_boot.text]
 
 async def envia_mensagem(mensagem):
     try:
@@ -47,11 +50,17 @@ async def envia_mensagem(mensagem):
 
 for ip_print in IP_PRINTS:
     try:
-        saida+=nivel_tooner(ip_print)+"\n"
-        reinicia(ip_print)
+        nivel=nivel_tooner(ip_print)
+        print(nivel)
+        #if int(nivel[1])<500:
+        niveis_tooner.append(nivel)
+        saida+=f"{nivel[0]} {nivel[1]} {nivel[2]}\n"
+        #reinicia(ip_print)
     except Exception as e:
         print(f"Erro ao acessar {ip_print}: {str(e)}")
 
 driver.quit()
+
 print(saida)
+#if saida != "Nível de tooner das impressoras:\n":
 asyncio.run(envia_mensagem(saida))
